3fcab539ba7a0af70066db68d26f936921fc34f4
Ravindu Dilshan <ravindu.dilshan.rd@gmail.com>
2020-02-12 12:12:30 +0530

    examples to for loop

--- forLoop.py
+++ forLoop.py
@@ -1,10 +1,11 @@
-#x = [1,3,4,5,6,7,8,9,900,4,0]
-#x = "ravindu"
+#print elements in list one by one using for loop
+x = [1,3,4,5,6,7,8,9,900,4,0]
+for i in x:
+    print(i)
 
-#for i in x:
-#    print(i)
-
-name = "ravindu"
-
-for i in range(10,0,-1):
-    print(i,name)
+print() #new line
+#print a message given numbers
+message = "Hello World"
+for i in range(10):
+    print(message) 
+	#print for 10 times
