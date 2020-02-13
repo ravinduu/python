d8bb7e117a0af87c856853c01e5e4f861b5b1960
Ravindu Dilshan <ravindu.dilshan.rd@gmail.com>
2020-02-13 08:47:59 +0530

    break,continue and pass in a loop

--- break_continue_pass.py
+++ break_continue_pass.py
@@ -1,28 +1,24 @@
 #break
-#x = int(input("How many candy: "))
-
-#av = 10
-#i = 1
-#while i <= x:
-#    if i > av:
-#        print()
-#        break
-
-#    print("Candy")
-#    i+=1
+x = int(input("How many candy: "))
+av = 10
+i = 1
+while i <= x:
+    if i > av: #if this condition is true the loop will stop
+        print()
+        break
+    print("Candy")
+    i+=1
 
 #continue
-#for i in range(100):
-#	if i%3 == 0 or i%5 == 0:
-#		continue
-#
-#	print(i)
+for i in range(100):
+	if i%3 == 0 or i%5 == 0:#if this condition is true the loop will hop to next value
+		continue
+	print(i)
 
 #pass
 for i in range(100):
 	if i%2==0:
 		pass
-
 	else:
 		print(i)
 #pass is same as continue
