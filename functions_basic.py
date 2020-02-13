d27dc2d4d045b366e894c16d36113d8a8ddf77f2
Ravindu Dilshan <ravindu.dilshan.rd@gmail.com>
2020-02-13 08:40:35 +0530

    functions in python

--- functions_basic.py
+++ functions_basic.py
@@ -1,18 +1,22 @@
+#normal function
 def hello_world() :#define function call hello_world
 	print("Hello World")
 
 hello_world() #call the above function
 
-def add(x,y) : #with arguments
+#function with arguments
+def add(x,y) : #x,y are arguments
 	print(x,"+",y,"=",x+y)
 
 add(3,4)
-	
+
+#function with return a value
 def diff(x,y):
 	return x+y #return value
 
 print(diff(34,43))
 
+#function with return multiple values
 def add_sub(x,y):
 	return x+y,x-y#return 2 values
 
