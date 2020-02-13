e6b590c4031e1b0051779aad78a41bb243d2b82c
Ravindu Dilshan <ravindu.dilshan.rd@gmail.com>
2020-02-13 08:41:23 +0530

    arguments in functions

--- function_args.py
+++ function_args.py
@@ -1,3 +1,4 @@
+#normal function that update given value
 def update(x) :
 	x = 8
 	print(x)
@@ -6,19 +7,20 @@ a = 10
 update(a)
 print(a)
 
-def persion(name,age = 16) :#in here the sequance of args is important 
+#function with two arguments
+def persion(name,age = 16) :#in here the sequance of args are important 
 	print(name)
 	print(age)
 
-persion("ravindu",23)
-persion(age = 32,name = "dilshan")#keyword args
-persion("name")#age is default set to 16
+persion("name1",23)#normal way to call the function
+persion(age = 32,name = "name2")#call the function using keyword args
+persion("name3")#age is default set to 16
 
 
 #def sum(a,*b) : # *b is to multiple vales... can pass any values
 
-def persion_details(name,**data) : #in here when we pass the data we need to specify the keyword
+def persion_details(name,**data) : #in here when we pass the data we need to specify the keyword ,by using ** we can pass multiple args
 	print(name)
-	print(data)
+	print(data)# the ** will take values as a list
 
-persion_details("ravindu",age=23,uni="UOP",school="SSCK",major="CS")
+persion_details("name",age=23,uni="uni",school="school",major="major")#name is required,others are user define, but need to use keywords
